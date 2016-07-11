bb96995dd82b8bb457ecbc06be9aea03c57d5f28
Robert Prine <robertprine2@yahoo.com>
2016-07-11 09:24:36 -0400

    made a winform calculator and wasn't sure if I was going to have time to finish

---
+++ CalculatorWinForm/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace CalculatorWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.one = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.period = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.equals = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.clearCurrent = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.problem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // seven
+            // 
+            this.seven.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seven.Location = new System.Drawing.Point(12, 138);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(50, 50);
+            this.seven.TabIndex = 0;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.button_click);
+            // 
+            // eight
+            // 
+            this.eight.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eight.Location = new System.Drawing.Point(68, 138);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(50, 50);
+            this.eight.TabIndex = 1;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.button_click);
+            // 
+            // nine
+            // 
+            this.nine.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nine.Location = new System.Drawing.Point(124, 138);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(50, 50);
+            this.nine.TabIndex = 2;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.button_click);
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divide.Location = new System.Drawing.Point(180, 138);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(50, 50);
+            this.divide.TabIndex = 3;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.operator_click);
+            // 
+            // one
+            // 
+            this.one.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.one.Location = new System.Drawing.Point(12, 250);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(50, 50);
+            this.one.TabIndex = 4;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.button_click);
+            // 
+            // six
+            // 
+            this.six.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.six.Location = new System.Drawing.Point(124, 194);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(50, 50);
+            this.six.TabIndex = 5;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.button_click);
+            // 
+            // multiply
+            // 
+            this.multiply.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiply.Location = new System.Drawing.Point(180, 194);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(50, 50);
+            this.multiply.TabIndex = 6;
+            this.multiply.Text = "*";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.operator_click);
+            // 
+            // five
+            // 
+            this.five.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.five.Location = new System.Drawing.Point(68, 194);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(50, 50);
+            this.five.TabIndex = 7;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.button_click);
+            // 
+            // four
+            // 
+            this.four.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.four.Location = new System.Drawing.Point(12, 194);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(50, 50);
+            this.four.TabIndex = 8;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.button_click);
+            // 
+            // two
+            // 
+            this.two.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.two.Location = new System.Drawing.Point(68, 250);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(50, 50);
+            this.two.TabIndex = 9;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.button_click);
+            // 
+            // three
+            // 
+            this.three.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.three.Location = new System.Drawing.Point(124, 250);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(50, 50);
+            this.three.TabIndex = 10;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.button_click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.Location = new System.Drawing.Point(180, 250);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(50, 50);
+            this.minus.TabIndex = 11;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.operator_click);
+            // 
+            // zero
+            // 
+            this.zero.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zero.Location = new System.Drawing.Point(12, 306);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(106, 50);
+            this.zero.TabIndex = 12;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.button_click);
+            // 
+            // period
+            // 
+            this.period.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.period.Location = new System.Drawing.Point(124, 306);
+            this.period.Name = "period";
+            this.period.Size = new System.Drawing.Size(50, 50);
+            this.period.TabIndex = 14;
+            this.period.Text = ".";
+            this.period.UseVisualStyleBackColor = true;
+            this.period.Click += new System.EventHandler(this.button_click);
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(180, 306);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(50, 50);
+            this.add.TabIndex = 15;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.operator_click);
+            // 
+            // equals
+            // 
+            this.equals.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equals.Location = new System.Drawing.Point(236, 250);
+            this.equals.Name = "equals";
+            this.equals.Size = new System.Drawing.Size(50, 106);
+            this.equals.TabIndex = 19;
+            this.equals.Text = "=";
+            this.equals.UseVisualStyleBackColor = true;
+            this.equals.Click += new System.EventHandler(this.equals_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(236, 194);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(50, 50);
+            this.clear.TabIndex = 17;
+            this.clear.Text = "C";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // clearCurrent
+            // 
+            this.clearCurrent.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearCurrent.Location = new System.Drawing.Point(236, 138);
+            this.clearCurrent.Name = "clearCurrent";
+            this.clearCurrent.Size = new System.Drawing.Size(50, 50);
+            this.clearCurrent.TabIndex = 16;
+            this.clearCurrent.Text = "CE";
+            this.clearCurrent.UseVisualStyleBackColor = true;
+            this.clearCurrent.Click += new System.EventHandler(this.clearCurrent_Click);
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(12, 38);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(274, 31);
+            this.result.TabIndex = 20;
+            this.result.Text = "0";
+            this.result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // problem
+            // 
+            this.problem.AutoSize = true;
+            this.problem.BackColor = System.Drawing.SystemColors.Window;
+            this.problem.Font = new System.Drawing.Font("Old English Text MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.problem.Location = new System.Drawing.Point(12, 9);
+            this.problem.Name = "problem";
+            this.problem.Size = new System.Drawing.Size(0, 23);
+            this.problem.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 368);
+            this.Controls.Add(this.problem);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.equals);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.clearCurrent);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.period);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Awesome Calculator!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button period;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button equals;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button clearCurrent;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label problem;
+    }
+}
+
---
+++ CalculatorWinForm/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorWinForm
+{
+    public partial class Form1 : Form
+    {
+        // The answer that is computed and then changed to a string
+        Double resultAmount = 0;
+        // operator that was clicked
+        String operatorUsed = "";
+        // keeps track so people don't use two operators next to eachother
+        bool isOperationPerformed = false;
+        // allows for clearing of the typing screen after equal button is pressed
+        bool justPressedEquals = false;
+        // makes it so that you can't press an operator button twice
+        bool justPressedOperator = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // when numbers or . are clicked
+        private void button_click(object sender, EventArgs e)
+        {
+            // if there is currently a 0 on screen or the last button clicked was an operator
+            if ((result.Text == "0") || (isOperationPerformed))
+            {
+                result.Clear();
+            }
+            // changes the bool to false so that you knew a number or decimal has been clicked most recently and another operator can be used
+            isOperationPerformed = false;
+            // the text that is being sent when the user clicks on a button
+            Button button = (Button)sender;
+            // if the . button is clicked
+            if (button.Text == ".")
+            {
+                // if the result doesn't have a . yet add one, but otherwise don't do anything
+                if (!result.Text.Contains("."))
+                {
+                    result.Text += button.Text;
+                    problem.Text += button.Text;
+                    justPressedOperator = false;
+                }
+            }
+            // clears typing screen if the last button pressed was equals
+            else if (justPressedEquals)
+            {
+                result.Text = "";
+                justPressedEquals = false;
+                result.Text += button.Text;
+                problem.Text += button.Text;
+                justPressedOperator = false;
+            }
+            // add the clicked button's text to the problem view and result view
+            else
+            {
+                result.Text += button.Text;
+                problem.Text += button.Text;
+                justPressedOperator = false;
+            }
+        }
+        // method for when you click an operator
+        private void operator_click(object sender, EventArgs e)
+        {
+            // The button's text that is being sent
+            Button button = (Button)sender;
+            // if an operator has just been pressed do nothing
+            if (justPressedOperator == false)
+            {
+                if (resultAmount != 0)
+                {
+                    // solves the problem so far and puts it in the typing view
+                    equals.PerformClick();
+                    operatorUsed = button.Text;
+                    problem.Text = resultAmount + " " + operatorUsed;
+                    isOperationPerformed = true;
+                    justPressedOperator = true;
+                }
+                else
+                {
+                    // just puts the button text into the typing view
+                    operatorUsed = button.Text;
+                    // moves string in calculator view to resultAmount Variable and changes it to a double
+                    resultAmount = Double.Parse(result.Text);
+                    problem.Text += operatorUsed;
+                    isOperationPerformed = true;
+                    justPressedOperator = true;
+                }
+            }
+        }
+        // clears the history and the current typing view
+        private void clear_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+            resultAmount = 0;
+            justPressedOperator = false;
+        }
+        // clears just the typing view so you don't have to retype everything
+        private void clearCurrent_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+            justPressedOperator = false;
+        }
+        // solves problems entered into calculator
+        private void equals_Click(object sender, EventArgs e)
+        {
+            // finds the operator that was clicked to solve the problem correctly
+            switch(operatorUsed)
+            {
+                case "+":
+                    resultAmount = (resultAmount + Double.Parse(result.Text));
+                    break;
+                case "-":
+                    resultAmount = (resultAmount - Double.Parse(result.Text));
+                    break;
+                case "*":
+                    resultAmount = (resultAmount * Double.Parse(result.Text));
+                    break;
+                case "/":
+                    resultAmount = (resultAmount / Double.Parse(result.Text));
+                    break;
+                default:
+                    break;
+            } // end switch
+            // prints answer to problem in typing screen
+            result.Text = resultAmount.ToString();
+            resultAmount = Double.Parse(result.Text);
+            // resets the history
+            problem.Text = "";
+            // allows the next button click to clear the typing screen
+            justPressedEquals = true;
+            // resets the equations answer amount
+            resultAmount = 0;
+            justPressedOperator = false;
+        }
+    }
+}
